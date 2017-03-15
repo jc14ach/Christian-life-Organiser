d052d5888a4e2e4a777fa81568d040b253e39fd4
jc14ach <jjcritoph@hotmail.co.uk>
2017-03-15 14:20:44 +0000

    Code 6
    Reminder

--- Christian life Organiser/ThirdView.swift
+++ Christian life Organiser/ThirdView.swift
@@ -7,23 +7,57 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ThirdView: UIViewController {
     
-    override func viewDidLoad() {
+    
+    
+    
+    
+  override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
     
-    
-    
-    
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
+    
+    @IBOutlet weak var datePickerBile: UIDatePicker!
+    
+    
+    @IBAction func bibleReminder(_ sender: UIButton) {
+    
+        // 1
+        let notification = UILocalNotification()
+        // 2
+        notification.fireDate = datePickerBile.date
+        // 3
+        notification.alertBody = "reminder: read the bible"
+        
+            
+        
+        // 4
+        notification.timeZone = NSTimeZone.default
+        // 5
+        notification.repeatInterval = NSCalendar.Unit.month
+        // 6
+        notification.applicationIconBadgeNumber = 1
+       
+       func updateUI(){
+        let currentSettings = UIApplication.shared.scheduleLocalNotification(notification)
+        }
+
+        
+    }
+    
+    
+
 }
+
